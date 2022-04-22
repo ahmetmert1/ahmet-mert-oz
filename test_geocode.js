8218240af1608dc2e07e1bfc8a7dbb5405884894
ahmetmert1 <18360859037@ogrenci.btu.edu.tr>
2022-04-22 09:56:31 +0300

    api yazimi

--- test_geocode.js
+++ test_geocode.js
@@ -1,2 +1,18 @@
 const request = require("request")
 
+const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(adress) + ".json?access_token=pk.eyJ1IjoiaWZzNSIsImEiOiJjbDFveGZ2cjIxNjV3M2tvMm1kaW45MjF3In0.jakzckr8Zkj8Xdbo8yQ05w"
+
+
+const geocode = (callback)=>{
+
+    const veriAl = request({url : url, json : true},(error,data)=>{
+
+        if(error){
+            callback(error,undefined)
+        }else{
+            callback(undefined,data)
+        }
+
+    })
+
+}
